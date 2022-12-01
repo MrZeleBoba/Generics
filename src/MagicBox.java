be1df1902b17985a2536df8ca25d1987e602e99e
Aleksey Galishchev <galishchev@inbox.ru>
2022-12-01 11:52:50 +0300

    Correction

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,11 +1,14 @@
+
 import java.util.Random;
 
 public class MagicBox<T> {
     protected T[] items;
-    Random random = new Random();
+    protected int maxQuantity;
 
     public MagicBox(int maxQuantity) {
-        this.items = (T[]) new Object[maxQuantity];
+        T[] items = (T[]) new Object[maxQuantity];
+        this.items = items;
+        this.maxQuantity = maxQuantity;
     }
 
     public boolean add(T item) {
@@ -24,10 +27,24 @@ public class MagicBox<T> {
             if (items[i] == null) {
                 throw new RuntimeException("Необходимо заполнить" + " " + (items.length - i) + " " + "ячеек");
             }
-            int randomInt = random.nextInt(items.length);
+
+
         }
-        return items[random.nextInt(3)];
+        Random random = new Random();
+        int randomInt = random.nextInt(maxQuantity);
+        return items[randomInt];
 
     }
-}
+
+
+
+
+    }
+
+
+
+
+
+
+
 
